9e394bfcc316bde84210cdbf5b0d6ddc9413d1db
t-hin241 <114737131+t-hin241@users.noreply.github.com>
2022-10-04 14:37:05 +0700

    Delete Segment Tree directory

--- Segment Tree/test.py
+++
@@ -1,9 +0,0 @@
-from segment_tree import *
-import random
-from draw_tree import *
-
-array_length = 10
-number_of_queries = 100
-array = [random.randint(-10, 30) for i in range(array_length)]
-t = SegmentTree(array)
-visualize(t.root)
